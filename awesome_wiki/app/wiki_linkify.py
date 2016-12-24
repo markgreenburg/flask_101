7025b45b12819135c099bafdc08d6e05100af226
markgreenburg <marek.greenburg@gmail.com>
2016-12-23 19:07:31 -0600

    Fixed minor content display and edit issue with leading chars

--- awesome_wiki/app/wiki_linkify.py
+++ awesome_wiki/app/wiki_linkify.py
@@ -1,14 +1,23 @@
-# How to use
-# >>> from wiki_linkify import wiki_linkify
-# >>> wiki_linkify('I <3 DigitalCrafts!')
-# 'I <3 <a href="/DigitalCrafts">DigitalCrafts</a>!'
+"""
+How to use
+>>> from wiki_linkify import wiki_linkify
+>>> wiki_linkify('I <3 CamelCase!')
+'I <3 <a href="/CamelCase">CamelCase</a>!'
+"""
 
 import re
 
 def _replace(word):
+    """
+    Replaces words with links
+    """
     return '<a href="/%s">%s</a>' % (word.group(0), word.group(0))
 
 def wiki_linkify(string):
+    """
+    Finds CamelCase words within a given string and calls _replace to Replace
+    only those words with their equivalent links
+    """
     return re.sub('([A-Z][a-z]+){2,}', _replace, string)
 
 if __name__ == '__main__':
